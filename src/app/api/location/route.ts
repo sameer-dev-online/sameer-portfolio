ad024357dd2dac0579e11ffeb1e98312938820a5
Sameer Ahmed <sameer.3for4@gmail.com>
2025-10-06 23:52:38 -0700

    location endpoint bug fixed.

--- src/app/api/location/route.ts
+++ src/app/api/location/route.ts
@@ -1,15 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const ip =
-    request.headers.get("x-forwarded-for") ||
-    request.headers.get("x-real-ip") ||
-    "8.8.8.8"; // fallback for local dev;
 
-  const res = await fetch(`https://ipapi.co/${ip}/json/`);
+    const forwardedFor = request.headers.get('x-forwarded-for');
+     const realIp =
+    forwardedFor?.split(',')[0].trim() || // multiple IPs se first lelo
+    request.headers.get('x-real-ip') ||
+    request.headers.get('cf-connecting-ip') ||
+    'unknown';
+    console.log(realIp);
+  const res = await fetch(`https://ipapi.co/${realIp}/json/`);
   const data = await res.json();
   return NextResponse.json({
-    ip,
+    ip: realIp,
     country: data.country_name || "Pakistan",
     currency: data.currency || "PKR"
   });
